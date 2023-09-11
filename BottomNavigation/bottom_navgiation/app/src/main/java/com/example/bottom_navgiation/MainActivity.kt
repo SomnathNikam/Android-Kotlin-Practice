b8b161c7b4f1e946733b9847e24458d471b67c7c
SOMNATH <somnathnikam2076@gmail.com>
2023-09-11 06:27:55 +0530

    Initial Commit

---
+++ BottomNavigation/bottom_navgiation/app/src/main/java/com/example/bottom_navgiation/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bottom_navgiation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

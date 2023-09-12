0a41db02e9f8f39216601eb427c618a915332026
SOMNATH <somnathnikam2076@gmail.com>
2023-09-12 05:58:55 +0530

    removed Material design

--- BottomNavigation/Bottom_nav/app/src/main/java/com/example/bottom_nav/MainActivity.kt
+++ BottomNavigation/Bottom_nav/app/src/main/java/com/example/bottom_nav/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.bottom_nav
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+
 import com.example.bottom_nav.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,15 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         replaceFragment(HomeFragm())
 
         binding.bottomNav.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.home -> replaceFragment(HomeFragm())
-
                 R.id.info -> replaceFragment(InfoFragment())
                 R.id.trending -> replaceFragment(trendsFragment())
-
                 R.id.trending -> replaceFragment(SettingsFragment())
                 else -> {
 
